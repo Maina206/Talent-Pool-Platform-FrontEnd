fcde477297b47353113937f95b10d64c4307e06a
Alexander <maina206mwangi@gmail.com>
2025-02-06 03:19:40 +0300

    still testing render

--- src/authServices.jsx
+++ src/authServices.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000";
+const API_URL = "https://talent-pool-platform-backend.onrender.com";
 
 const authService = {
   // Existing employer login
